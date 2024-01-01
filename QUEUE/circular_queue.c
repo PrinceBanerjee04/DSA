ccdeebb83c75632e23b02b3c795a452760c52548
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-01-01 13:09:30 +0530

    Create circular_queue.c

--- QUEUE/circular_queue.c
+++ QUEUE/circular_queue.c
@@ -8,3 +8,17 @@ typedef struct queue
     int arr[size];
     int front, rear;
 } queue;
+
+int addcirq(int a, queue *q)
+{
+    if ((q->rear + 1) % size == q->front)
+        return 0; // Queue is full
+    else
+    {
+        if (q->front == -1) // If queue is initially empty
+            q->front = 0;
+        q->rear = (q->rear + 1) % size;
+        q->arr[q->rear] = a;
+        return 1; // Element added successfully
+    }
+}
